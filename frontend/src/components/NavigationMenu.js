1586aa03c606a11a0ed866cb4c9b0b04c709f29c
Kazuhiro <kuyama@systemdesignk2.jp>
2025-10-16 16:11:36 +0900

    Change Navigation Menue.

--- frontend/src/components/NavigationMenu.js
+++ frontend/src/components/NavigationMenu.js
@@ -16,32 +16,6 @@ const cookies = new Cookies();
 
 function NavigationMenu(props){
     const ctx = useContext(ShowPolicyContext);
-    const [username, setUser] = useState(null);
-
-    useEffect(() => {
-        // ユーザー名を取得する
-        if(Settings.DEVELOP){
-            return
-        }
-        if(Settings.FULL_FUNCTIONS){
-            const token = cookies.get('csrftoken')
-            //console.log("csrftoken:", token);
-            fetch(Settings.HOME_PATH+"/account/api-whoami",{
-                        credentials: "same-origin",
-                    }
-            )
-            .then(response => {
-                return response.json();
-            })
-            .then(result =>{
-                const txt = JSON.stringify(result, null,' ');
-                //console.log('---Success ---');
-                //console.log(txt);
-                let res = JSON.parse(txt);
-                setUser(res['username'])
-            })
-        }
-    },[])
 
     
     useEffect(() =>{
@@ -63,63 +37,6 @@ function NavigationMenu(props){
         })    
     },[])
 
-    const logout = (e) =>{
-        let LO_PATH="/api/logout/";
-        if(Settings.DELEGATION){
-            LO_PATH="/api/d_logout/";
-        } 
-
-        const token = cookies.get('csrftoken')
-        //console.log("token:", token);
-        let target = Settings.HOME_PATH
-        if(Settings.DEVELOP){
-            target = Settings.DEVELOPMENT_HOME_PATH
-        }
-        fetch(target+LO_PATH, {
-			method : "POST",
-			credentials: "same-origin",
-			headers: {
-			    'Content-Type' : 'application/json',
-			    'X-CSRFToken': token,
-			},
-		}
-		)
-		.then((response) =>{
-		    console.log(response);
-            if(!Settings.DELEGATION){
-		        window.location.href=response.url;
-            }else {
-                setUser(null);
-            }
-		})
-		.catch(error =>{
-			console.error(error);
-		})
-
-    }
-
-
-    const login = (e) =>{
-        const token = cookies.get('csrftoken')
-        console.log("token:", token);
-        fetch(Settings.HOME_PATH+"/api/d_login", {
-			method : "POST",
-			credentials: "same-origin",
-			headers: {
-			    'Content-Type' : 'application/json',
-			    'X-CSRFToken': token,
-			},
-		}
-		)
-		.then((response) =>{
-		    //console.log(response);
-            setUser(response.username);
-		})
-		.catch(error =>{
-			console.error(error);
-		})
-    }
-
 
     if (Settings.FULL_FUNCTIONS) {    
         //console.log(s_context.policy);
@@ -127,8 +44,7 @@ function NavigationMenu(props){
         return(
             <Navbar expand="lg" className="bg-body-tertiary"  data-bs-theme="dark">
                     <Container>
-                        <Navbar.Brand href="#home">QT {username}</Navbar.Brand>
-                        {/* <div className="username"> {username} </div> */}
+                        <Navbar.Brand href="#home">QT</Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="mr-auto">            	
@@ -153,16 +69,7 @@ function NavigationMenu(props){
                                 <Nav.Link href={Settings.HOME_PATH+"/editor"}>カテゴリーエディター</Nav.Link> 
                                 
                                 <Nav.Link href={Settings.HOME_PATH+"/settings"}>設定</Nav.Link>
-                                <Nav.Link href={Settings.HOME_PATH+"/eval"}>AI評価</Nav.Link>
-                                {/*
-                                <NavDropdown title="データベース" id="category-nav-dropdown">
-                                    <NavDropdown.Item href={Settings.HOME_PATH+"/data_and_files"}>データアップロード</NavDropdown.Item>
-                                    <NavDropdown.Item href={Settings.HOME_PATH+"/db_migration"}>DBマイグレーション</NavDropdown.Item>
-                                </NavDropdown>
-                                */}
-                                
                             </Nav>
-                            <Button className="logout-button" variant="secondary" size="sm" style={{marginLeft: "2px"}} onClick={logout} id="loButton">ログアウト <i class="bi bi-box-arrow-right"></i></Button>
                         </Navbar.Collapse>
                     </Container>
                     
